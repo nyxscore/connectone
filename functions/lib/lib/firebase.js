89fe83a4647c6bb74c7421d65d340d42ca6887f5
nyxscore <nyxscore@gmail.com>
2025-10-11 01:31:16 +0900

    fix: Node.js 20 업그레이드 완료 및 functions 코드 간소화

--- functions/lib/lib/firebase.js
+++ functions/lib/lib/firebase.js
@@ -1,16 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.db = void 0;
-const app_1 = require("firebase/app");
-const firestore_1 = require("firebase/firestore");
-const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-};
-const app = (0, app_1.initializeApp)(firebaseConfig);
-exports.db = (0, firestore_1.getFirestore)(app);
+const firestore_1 = require("firebase-admin/firestore");
+const app_1 = require("firebase-admin/app");
+// Firebase Admin 초기화
+if (!(0, app_1.getApps)().length) {
+    (0, app_1.initializeApp)();
+}
+// Firestore 인스턴스
+exports.db = (0, firestore_1.getFirestore)();
 //# sourceMappingURL=firebase.js.map
